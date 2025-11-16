b666551758ca33c8a2a043918767b560c31aff6f
VYR4L <fkz.zanatt@gmail.com>
2025-11-16 02:39:20 -0300

    feat: add admin check for roles and routes for balances, categories, goals and transactions

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from api.routes import user_routes
+from api.routes import user_routes, balance_routes, categories_routes, goals_routes, transactions_routes
 import uvicorn
 from config import settings, Base
-from models import users
+from utils.permissions import verify_admin_token
 import os
 
 
@@ -24,6 +24,10 @@ app = FastAPI(
     description="API para gerenciamento de despesas pessoais.",
     version="1.0.0",
     debug=settings.DEBUG,
+    docs_url="/docs" if settings.DEBUG else None,
+    redoc_url="/redoc" if settings.DEBUG else None,
+    openapi_url="/openapi.json",
+    dependencies=[Depends(verify_admin_token)] if not settings.DEBUG else [],
 )
 
 app.add_middleware(
@@ -35,6 +39,10 @@ app.add_middleware(
 )
 
 app.include_router(user_routes.router)
+app.include_router(balance_routes.router)
+app.include_router(categories_routes.router)
+app.include_router(goals_routes.router)
+app.include_router(transactions_routes.router)
 
 @app.get("/")
 async def root():
@@ -45,11 +53,11 @@ async def root():
     }
 
 @app.get("/health")
-async def health_check():
+async def health_check(admin: bool = Depends(verify_admin_token)):
     return {"status": "healthy"}
 
 @app.get("/config")
-async def get_config():
+async def get_config(admin: bool = Depends(verify_admin_token)):
     return settings.get_info()
 
 if __name__ == "__main__":
